1095d9c9488dd24e2dd393c2f4d93a9180b67e9c
Orbittal <166711773+Orbittal24@users.noreply.github.com>
2025-03-11 13:14:12 +0530

    Update page.js

--- src/app/history_data/page.js
+++ src/app/history_data/page.js
@@ -1,1 +1,20 @@
+"use client";
 
+import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
+import TableOne from "@/components/Tables/TableOne";
+import TableTwo from "@/components/Tables/TableTwo";
+import TableThree from "@/components/Tables/TableThree";
+import { useEffect, useState } from "react";
+import DefaultLayout from "@/components/Layouts/DefaultLayout";
+
+const Page = () => {
+  return (
+    <DefaultLayout>
+      <div>
+        <h1>History Page</h1>
+      </div>
+    </DefaultLayout>
+  );
+};
+
+export default Page;
